300371d253f6b3475424c0bd76ebb4e8d1bc6021
amirr1360 <104555868+amirr1360@users.noreply.github.com>
2023-07-22 23:55:43 +0400

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x991c9478a1c68477907dbfe976febf4380480ad5";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
